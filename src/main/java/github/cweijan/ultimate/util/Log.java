626cc0bb39a0bf7ae9cce003800c20a2ac607b97
cweijan <cweijan@163.com>
2019-06-17 16:46:56 +0800

    日志工具类优化

--- src/main/java/github/cweijan/ultimate/util/Log.java
+++ src/main/java/github/cweijan/ultimate/util/Log.java
@@ -8,7 +8,12 @@ public class Log {
 
     private static Logger loggerChecker = LoggerFactory.getLogger(Log.class);
 
-    private static Logger getLogger() {
+    public static Logger getLogger(){
+
+        return getLoggerInner();
+    }
+
+    private static Logger getLoggerInner() {
         StackTraceElement stackTrace = (new Throwable()).getStackTrace()[2];
         return LoggerFactory.getLogger(stackTrace.getClassName());
     }
@@ -16,7 +21,7 @@ public class Log {
     @JvmStatic
     public static void error(Object content) {
         if (loggerChecker.isErrorEnabled()) {
-            getLogger().error(content != null ? content.toString() : null, "");
+            getLoggerInner().error(content != null ? content.toString() : null, "");
         }
 
     }
@@ -24,7 +29,7 @@ public class Log {
     @JvmStatic
     public static void error(Object content, Throwable throwable) {
         if (loggerChecker.isErrorEnabled()) {
-            getLogger().error(content != null ? content.toString() : null, "", throwable);
+            getLoggerInner().error(content != null ? content.toString() : null, "", throwable);
         }
 
     }
@@ -32,7 +37,7 @@ public class Log {
     @JvmStatic
     public static void warn(Object content) {
         if (loggerChecker.isWarnEnabled()) {
-            getLogger().warn(content != null ? content.toString() : null, "");
+            getLoggerInner().warn(content != null ? content.toString() : null, "");
         }
 
     }
@@ -40,7 +45,7 @@ public class Log {
     @JvmStatic
     public static void debug(Object content) {
         if (loggerChecker.isDebugEnabled()) {
-            getLogger().debug(content != null ? content.toString() : null, "");
+            getLoggerInner().debug(content != null ? content.toString() : null, "");
         }
 
     }
@@ -48,7 +53,7 @@ public class Log {
     @JvmStatic
     public static void info(Object content) {
         if (loggerChecker.isInfoEnabled()) {
-            getLogger().info(content != null ? content.toString() : null, "");
+            getLoggerInner().info(content != null ? content.toString() : null, "");
         }
 
     }
