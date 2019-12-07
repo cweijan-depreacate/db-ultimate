d2b9cca4f23936a5e5faaaab9ba724fecc9a4bd4
cweijan <cweijan@163.com>
2019-12-07 18:31:21 +0800

    1.4.93

--- src/main/java/github/cweijan/ultimate/core/dialect/BaseSqlDialect.kt
+++ src/main/java/github/cweijan/ultimate/core/dialect/BaseSqlDialect.kt
@@ -60,7 +60,7 @@ abstract class BaseSqlDialect : SqlDialect {
             return (fieldValue as Enum<*>).name
         }
         if(fieldValue.javaClass==Date::class.java){
-            return java.sql.Date((fieldValue as Date).time)
+            return java.sql.Timestamp((fieldValue as Date).time)
         }
         return fieldValue
     }
